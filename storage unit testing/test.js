fdfa4dc17bc08d29f0c4346c6ee39244bbab5505
Lwandile Ganyile <ganyilelwandilee@gmail.com>
2025-01-31 22:11:02 +0200

    Allow prevelaged users to perform CRUD operations on store owners related static content(images, pdfs etc).

--- storage unit testing/test.js
+++ storage unit testing/test.js
@@ -197,5 +197,436 @@ describe('Our Alcoholic App',()=>{
     
   });
 
+  //================================Store Owner Images[Start]==================================
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Offline User : Do not allow not logged in users to upload a store owner image.', async()=>{
+    
+    await assertFails(
+      noUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${someId}`).put(file5MB)
+      .then()
+    );
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Online User : Do not allow logged in users to upload a store owner image if it document id is not the same as the store owner document id[2].', async()=>{
+    
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${someId}`)
+      .put(file5MB).then()
+    );
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Online User : Do not allow logged in users to upload a store owner image if the store does not already exist[4].', async()=>{
+    
+    await assertFails(
+      theirUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${theirUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Online User : Do not allow logged in users to upload a store owner image if the store owner already exist[5].', async()=>{
+    
+    const ownerData = {
+      phoneNumber: myUserData.phoneNumber,
+      fullname: 'Nomusa',
+      surname:'Sukude',
+      profileImageURL: `../${myUserData.phoneNumbe}.jpg`,
+      identityDocumentImageURL: `../${myUserData.phoneNumbe}.jpg`,
+      isAdmin: false,
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('store_owners')
+      .doc(ownerData.phoneNumber).set(ownerData);
+    });
+
+    
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${myUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Online User : Do not allow logged in users to upload a store owner image with huge size[6].', async()=>{
+    
+    const storeData = {
+      storeOwnerPhoneNumber: myUserData.phoneNumber,
+      storeImageURL: '../mathayini_image.jpg',
+      storeName: 'Mathayini',
+      sectionName: 'Cato Crest-Mayville-Durban-Kwa Zulu-Natal-South Africa',
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('stores')
+      .doc(storeData.storeOwnerPhoneNumber).set(storeData);
+    });
+
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${myUserData.phoneNumber}`)
+      .put(file6MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Online User : Allow logged in users to upload a store owner image if the store already exist[7].', async()=>{
+    
+    const storeData = {
+      storeOwnerPhoneNumber: theirUserData.phoneNumber,
+      storeImageURL: '../mathayini_image.jpg',
+      storeName: 'Mathayini',
+      sectionName: 'Cato Crest-Mayville-Durban-Kwa Zulu-Natal-South Africa',
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('stores')
+      .doc(storeData.storeOwnerPhoneNumber).set(storeData);
+    });
+
+    await assertSucceeds(
+      theirUser.storage().ref()
+      .child(`/store_owners/store_owners_images/${theirUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_images/{storeOwnerImageId}
+  it('Store Owner : Do not allow store owners to upload store owner images.', async()=>{
+    
+    await assertFails(
+      storeOwnerUser.storage().ref()
+      .child('/store_owners/store_owners_images/'+storeOwnerData.phoneNumber)
+      .put(file5MB).then()
+    );
+  }); 
+
+
+
+
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_images/{storeOwnerImageId}
+  it('Offline User : Allow not logged in users to view store owner images.', async()=>{
+    
+    const storage = noUser.storage();
+      const storageRef = storage.ref(
+       `/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertSucceeds(storageRef.getDownloadURL());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_images/{storeImageId}
+  it('Online User : Allow logged in users to view store owner images.', async()=>{
+    
+    const storage = myUser.storage();
+      const storageRef = storage.ref()
+      .child(`/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`);
+
+    await assertSucceeds(storageRef.getDownloadURL());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_images/{storeOwnerImageId}
+  it('Store Owner : Allow store owners to view store owner images.', async()=>{
+    
+    const storage = storeOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertSucceeds(storageRef.getDownloadURL());
+  }); 
+
+
+
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/stores_images/{storeOwnerImageId}
+  it('Offline User : Do not allow not logged in users to delete store images.', async()=>{
+
+    const storage = noUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_images/{storeOwnerImageId}
+  it('Online User : Do not allow logged in users to delete store owner images.', async()=>{
+
+    const storage = myUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_images/{storeImageId}
+  it('Store Owner : Do not allow store owners to delete store owners images.', async()=>{
+
+    const storage = storeOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_images/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  });
+
+  //================================Store Owner Images[End]==================================
+
+ //================================Store Owner Id Images[Start]==================================
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Offline User : Do not allow not logged in users to upload a store owner identity document image.', async()=>{
+    
+    await assertFails(
+      noUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${someId}`).put(file5MB)
+      .then()
+    );
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to upload a store owner identity document image if it document id is not the same as the store owner document id[1].', async()=>{
+    
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to upload a store owner identity document image if the store does not already exist[4].', async()=>{
+    
+    await assertFails(
+      theirUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${theirUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to upload a store owner identity document image if the store owner already exist[5].', async()=>{
+    
+    const ownerData = {
+      phoneNumber: myUserData.phoneNumber,
+      fullname: 'Nomusa',
+      surname:'Sukude',
+      profileImageURL: `../${myUserData.phoneNumbe}.jpg`,
+      identityDocumentImageURL: `../${myUserData.phoneNumber}.jpg`,
+      isAdmin: false,
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('store_owners')
+      .doc(ownerData.phoneNumber).set(ownerData);
+    });
+
+    
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${myUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to upload a store owner identity document image with huge size[6].', async()=>{
+    
+    const storeData = {
+      storeOwnerPhoneNumber: myUserData.phoneNumber,
+      storeImageURL: '../mathayini_image.jpg',
+      storeName: 'Mathayini',
+      sectionName: 'Cato Crest-Mayville-Durban-Kwa Zulu-Natal-South Africa',
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('stores')
+      .doc(storeData.storeOwnerPhoneNumber).set(storeData);
+    });
+
+    await assertFails(
+      myUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${myUserData.phoneNumber}`)
+      .put(file6MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Allow logged in users to upload a store owner identity document image if the store already exist[7].', async()=>{
+    
+    const storeData = {
+      storeOwnerPhoneNumber: theirUserData.phoneNumber,
+      storeImageURL: '../mathayini_image.jpg',
+      storeName: 'Mathayini',
+      sectionName: 'Cato Crest-Mayville-Durban-Kwa Zulu-Natal-South Africa',
+    };
+
+    await testEnv.withSecurityRulesDisabled(context=>{
+      return context.firestore().collection('stores')
+      .doc(storeData.storeOwnerPhoneNumber).set(storeData);
+    });
+
+    await assertSucceeds(
+      theirUser.storage().ref()
+      .child(`/store_owners/store_owners_ids/${theirUserData.phoneNumber}`)
+      .put(file5MB).then()
+    );
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owner_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Do not allow store owners to upload store owner identity document images.', async()=>{
+    
+    await assertFails(
+      storeOwnerUser.storage().ref()
+      .child('/store_owners/store_owners_ids/'+storeOwnerData.phoneNumber)
+      .put(file5MB).then()
+    );
+  }); 
+
+
+
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Offline User : Do not allow not logged in users to view store owner identity documents.', async()=>{
+    
+    const storage = noUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.getDownloadURL());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to view store owner identity documents.', async()=>{
+    
+    const storage = myUser.storage();
+      const storageRef = storage.ref()
+      .child(`/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`);
+
+    await assertFails(storageRef.getDownloadURL());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Do not allow store owners to view store owner identity documents not belogning to them.', async()=>{
+    
+    const storage = theirStoreOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.getDownloadURL());
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Do not allow store owners to view store owner identity documents not belogning to them.', async()=>{
+    
+    const storage = theirStoreOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${storeOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.getDownloadURL());
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Allow store owners to view their store owner identity document.', async()=>{
+    
+    const storage = theirStoreOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertSucceeds(storageRef.getDownloadURL());
+  });
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Allow store owners who are admins to view any store owner identity document.', async()=>{
+    
+    const storage = storeOwnerUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertSucceeds(storageRef.getDownloadURL());
+  });
+
+
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Offline User : Do not allow not logged in users to delete store identity document.', async()=>{
+
+    const storage = noUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Online User : Do not allow logged in users to delete store owner identity document.', async()=>{
+
+    const storage = myUser.storage();
+      const storageRef = storage.ref(
+        `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  }); 
+
+  // Branch : store_resources_crud ->  store_resources_crud_storage_security_unit_testing
+  // Testing /store_owners/store_owners_ids/{storeOwnerIdentityDocumentId}
+  it('Store Owner : Do not allow store owners to delete store identity document.', async()=>{
+
+    const storage = storeOwnerUser.storage();
+      const storageRef = storage.ref(
+       `/store_owners/store_owners_ids/${theirStoreOwnerData.phoneNumber}`,
+      );
+
+    await assertFails(storageRef.delete());
+  });
+
+  //================================Store Owner Id Images[End]==================================
+
+
 });
 
